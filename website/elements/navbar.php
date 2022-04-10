4723fdd5bc0e1095482ee3048f80747051863aa2
Erlantz <garate.erlantz@uni.eus>
2022-04-11 00:30:37 +0200

    website includes footer and navbar

---
+++ website/elements/navbar.php
@@ -0,0 +1,44 @@
+<html lang="en">
+    <body>
+        <nav class="navbar navbar-expand-sm bg-light navbar-light sticky-top mainnav">
+            <a class="navbar-brand" href="./index.html">DIY garage</a>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#collapsibleNavbar"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href=""><span class="spanlink">Home</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href=""
+                            ><span class="spanlink">Reserve</span></a
+                        >
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href=""
+                            ><span class="spanlink">About us</span></a
+                        >
+                    </li>
+                </ul>
+                <ul class="navbar-nav navbar-right">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"
+                            ><span class="spanlink">Sign up</span></a
+                        >
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"
+                            ><span class="spanlink">Log in</span></a
+                        >
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </body>
+</html>
---
+++ website/index.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<title>uni2</title>
+		<link rel="stylesheet" href="styles.css" />
+		<!--BOOTSTRAP 4-->
+		<link
+			rel="stylesheet"
+			href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+		/>
+		<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+	</head>
+	<body>
+		
+		<div class="main">
+		<?php include ("./elements/navbar.php"); ?>
+			this page isn't ready
+		<?php include ("./elements/footer.html"); ?>
+		</div>
+	</body>
+</html>
