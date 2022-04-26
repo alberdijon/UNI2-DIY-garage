1c7a6b062df99ccb9b2cc9ff8aed3a5c34214440
Erlantz <96289199+GitPushErlantz@users.noreply.github.com>
2022-04-26 14:14:57 +0200

    reserve has been created

--- website/reserve.php
+++ website/reserve.php
@@ -8,7 +8,19 @@
 		<div class="main">
 		<?php include ("./elements/navbar.php"); ?>
         <div class="space"></div>
-			this page isn't ready
+			<h1 class="title">Make a reservation</h1>
+            <div class="reservemain">
+                <div class="rdatetime">
+                    <h3>Date and time</h3>
+                </div>
+                <div class="rgarage">
+                    <h3>Select a cabin</h3>
+                </div>
+                <div class="rconfirm">
+                    <h3>Price</h3>
+                    <button class="greenbutton" type="submit">Confirm</button>
+                </div>
+            </div>
 		<?php include ("./elements/footer.html"); ?>
 		</div>
 	</body>
