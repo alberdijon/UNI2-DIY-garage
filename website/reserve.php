9d9c6f6bcc44c7512817d9b0e847e76153b58f10
Erlantz <96289199+GitPushErlantz@users.noreply.github.com>
2022-04-27 08:11:20 +0200

    reserve ui started

--- website/reserve.php
+++ website/reserve.php
@@ -15,6 +15,16 @@
                 </div>
                 <div class="rgarage">
                     <h3>Select a cabin</h3>
+                    <div class="rgraphic">
+                        <div class="rgcabin cabin">1</div>
+                        <div class="rgcabin cabin">2</div>
+                        <div class="rgcabin cabin">3</div>
+                        <div class="rgvcabin">
+                            <div class="rgmcabin cabin">4</div>
+                            <div class="rgmcabin cabin">5</div>
+                            <div class="rgmcabin cabin">6</div>
+                        </div>
+                    </div>
                 </div>
                 <div class="rconfirm">
                     <h3>Price</h3>
