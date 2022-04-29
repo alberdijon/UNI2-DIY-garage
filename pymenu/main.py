2e33c2e28ec1ef33e10dff17c7592c191dd99e18
alberdijon <96289672+alberdijon@users.noreply.github.com>
2022-04-29 11:00:06 +0200

    pymenu initial commit

---
+++ pymenu/main.py
@@ -0,0 +1,22 @@
+from Users import *
+
+def main_menu():
+    print("1. users")
+    print("2. employees")
+    print("3. cabins")
+    print("4. shop")
+    print("5. exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        users_menu()
+    elif choice == "2":
+        employees_menu()
+    elif choice == "3":
+        cabins_menu()
+    elif choice == "4":
+        employees_menu()
+    elif choice == "5":
+        shop_menu()
+    else:
+        print("Invalid choice")
+        menu()
