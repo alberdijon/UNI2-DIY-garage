8cecda47bd57c77a62c455ba1fa414808da6a950
Erlantz <garate.erlantz@uni.eus>
2022-05-04 21:39:54 +0200

    py changes

--- pymenu/users.py
+++ pymenu/users.py
@@ -1,4 +1,7 @@
-class users:
+import pickle
+
+
+class User:
     def __init__(self, first_name, last_name, email, password, phone_number):
         self.first_name = first_name
         self.last_name = last_name
@@ -31,33 +34,27 @@ def users_menu():
 
 
 def view_users():
-    with open("users.txt", "r") as f:
-        lines = f.readlines()
-    for line in lines:
-        print(line.strip("\n"))
+    # Open the users.pkl file
+    with open("users.pkl", "rb") as f:
+        pklusers = pickle.load(f)
+    # Print the users
+    for user in pklusers:
+        print(user)
     users_menu()
 
 
 def add_user():
-    first_name = input("Enter first name: ")
-    last_name = input("Enter last name: ")
-    email = input("Enter email: ")
-    password = input("Enter password: ")
-    phone_number = input("Enter phone number: ")
-    user = users(first_name, last_name, email, password, phone_number)
-    with open("users.txt", "a") as f:
-        f.write(f"{user.first_name} {user.last_name} {user.email} {user.password} {user.phone_number}\n")
-    users_menu()
-
-
-def delete_user():
-    email = input("Enter email: ")
-    with open("users.txt", "r") as f:
-        lines = f.readlines()
-    with open("users.txt", "w") as f:
-        for line in lines:
-            if line.strip("\n") != email:
-                f.write(line)
+    # create a new user
+    new_user = User(input("Enter your first name: "), input("Enter your last name: "), input("Enter your email: "), input("Enter your password: "), input("Enter your phone number: "))
+    # Open the users.pkl file
+    with open("users.pkl", "rb") as f:
+        pklusers = pickle.load(f)
+    # Add the new user to the list
+    pklusers.append(new_user)
+    # Save the list to the users.pkl file
+    with open("users.pkl", "wb") as f:
+        pickle.dump(pklusers, f)
+    print("User added")
     users_menu()
 
 
