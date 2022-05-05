f20669fb73e6d19d50bb8d560055a95473ca768d
Erlantz <96289199+GitPushErlantz@users.noreply.github.com>
2022-05-05 08:12:15 +0200

    pickle works

--- pymenu/users.py
+++ pymenu/users.py
@@ -34,27 +34,44 @@ def users_menu():
 
 
 def view_users():
-    # Open the users.pkl file
-    with open("users.pkl", "rb") as f:
-        pklusers = pickle.load(f)
-    # Print the users
-    for user in pklusers:
-        print(user)
+    with open("users.pickle", "rb") as f:
+        try:
+            users = pickle.load(f)
+        except EOFError:
+            print("No users in database")
+            users = []
+    for user in users:
+        print(user.first_name, user.last_name, user.email, user.password, user.phone_number)
     users_menu()
 
 
 def add_user():
-    # create a new user
-    new_user = User(input("Enter your first name: "), input("Enter your last name: "), input("Enter your email: "), input("Enter your password: "), input("Enter your phone number: "))
-    # Open the users.pkl file
-    with open("users.pkl", "rb") as f:
-        pklusers = pickle.load(f)
-    # Add the new user to the list
-    pklusers.append(new_user)
-    # Save the list to the users.pkl file
-    with open("users.pkl", "wb") as f:
-        pickle.dump(pklusers, f)
-    print("User added")
+    first_name = input("Enter first name: ")
+    last_name = input("Enter last name: ")
+    email = input("Enter email: ")
+    password = input("Enter password: ")
+    phone_number = input("Enter phone number: ")
+    user = User(first_name, last_name, email, password, phone_number)
+    try:
+        with open("users.pickle", "rb") as f:
+            users = pickle.load(f)
+    except EOFError:
+        users = []
+    users.append(user)
+    with open("users.pickle", "wb") as f:
+        pickle.dump(users, f)
+    users_menu()
+
+
+def delete_user():
+    with open("users.pickle", "rb") as f:
+        users = pickle.load(f)
+    user_to_delete = input("Enter the email of the user you want to delete: ")
+    for user in users:
+        if user.email == user_to_delete:
+            users.remove(user)
+    with open("users.pickle", "wb") as f:
+        pickle.dump(users, f)
     users_menu()
 
 
