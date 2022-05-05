d5c5a07c610a77f10d98fff91057f4e713f43be8
Erlantz <96289199+GitPushErlantz@users.noreply.github.com>
2022-05-05 08:21:41 +0200

    pymenu betaq

--- pymenu/users.py
+++ pymenu/users.py
@@ -60,6 +60,7 @@ def add_user():
     users.append(user)
     with open("users.pickle", "wb") as f:
         pickle.dump(users, f)
+    print("User added")
     users_menu()
 
 
@@ -67,11 +68,47 @@ def delete_user():
     with open("users.pickle", "rb") as f:
         users = pickle.load(f)
     user_to_delete = input("Enter the email of the user you want to delete: ")
+    amount_deleted = 0
     for user in users:
         if user.email == user_to_delete:
+            print("A user has been deleted.")
+            amount_deleted += 1
             users.remove(user)
     with open("users.pickle", "wb") as f:
         pickle.dump(users, f)
+    if amount_deleted == 0:
+        print("No users found with that email")
+    else:
+        print("Deletion complete")
+    users_menu()
+
+
+def edit_user():
+    try:
+        with open("users.pickle", "rb") as f:
+            users = pickle.load(f)
+    except EOFError:
+        users = []
+        print("No users in database")
+    user_to_edit = input("Enter the email of the user you want to edit: ")
+    for user in users:
+        if user.email == user_to_edit:
+            print("User found")
+            first_name = input("Enter first name: ")
+            last_name = input("Enter last name: ")
+            email = input("Enter email: ")
+            password = input("Enter password: ")
+            phone_number = input("Enter phone number: ")
+            user.first_name = first_name
+            user.last_name = last_name
+            user.email = email
+            user.password = password
+            user.phone_number = phone_number
+            with open("users.pickle", "wb") as f:
+                pickle.dump(users, f)
+            print("User edited")
+            users_menu()
+    print("No users found with that email")
     users_menu()
 
 
