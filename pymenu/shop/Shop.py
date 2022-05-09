98c644d286a7fb084752ce3ad7c2b272b1c6306b
alberdijon <96289672+alberdijon@users.noreply.github.com>
2022-05-09 13:27:23 +0200

    ya funciona view

--- pymenu/shop/Shop.py
+++ pymenu/shop/Shop.py
@@ -1,18 +1,41 @@
+from importlib import import_module
 import pickle
 
 class Product:
     def __init__(self, id, name, price, stock, brand):
-        self.id = set
+        self.id = int(id)
+        self.name = str(name)
+        self.price = int(price)
+        self.stock = int(stock)
+        self.brand = str(brand)
+
+    def __str__(self):
+        return f"{self.id} - {self.name} - {self.price} - {self.stock} - {self.brand}"
+    def get_id(self):
+        return self.id
+    def get_name(self):
+        return self.name
+    def get_price(self):
+        return self.price
+    def get_stock(self):
+        return self.stock
+    def get_brand(self):
+        return self.brand
+    def set_id(self, id):
+        self.id = id
+    def set_name(self, name):
         self.name = name
+    def set_price(self, price):
         self.price = price
+    def set_stock(self, stock):
         self.stock = stock
+    def set_brand(self, brand):
         self.brand = brand
-
-
+    
 def Shop_menu():
     print("1. Add product")
     print("2. View all products")
-    print("3. Add stock")
+    print("3. Change stock")
     print("4. Edit product")
     print("5. Remove product")
     print("0. Exit")
@@ -69,23 +92,25 @@ def stock_menu():
     Shop_menu()
 
 def add_stock_to_product():
-    try:
-        with open("Products.pickle", "rb") as f:
-            products = pickle.load(f)
-    except EOFError:
-        products = []
-        print("No product in database")
-    product_to_edit = input("Enter the id of the product you want to add stock: ")
-    for Product in products:
+    inp=open('Products.pkl','rb')
+    objects = []
+    cont=1
+    while cont==1:
+        try:
+            objects.append(pickle.load(inp))
+        except EOFError:
+            print("end of products\n")
+            cont=0
+    product_to_edit = int(input("Enter the id of the product you want to add stock: "))
+    for Product in objects:
         if Product.id == product_to_edit:
             print("Product found")
-            stock = input("Enter how much stock you want to add: ")
-            Product.stock += stock
-            with open("Products.pickle", "wb") as f:
-                pickle.dump(products, f)
+            new_stock = input("Enter how much stock you want to add: ")
+            Product.stock += new_stock
+            with open('Products.pkl', 'wb') as inp:
+                pickle.dump(objects, f)
             print("Stock added")
-            Shop_menu()
-    print("No product found with that id")
+    Shop_menu()
 
 def remove_stock_from_product():
     try:
@@ -161,13 +186,6 @@ def add_product():
     brand = input("Enter brand: ")
     p1 = Product(id, name, price, stock, brand)
     products = []
-   # try:
-       # with open('Products.pkl', 'wb') as f:
-        #    products.append( pickle.load(f))
-    #except EOFError:
-     #   products = []*/
-    #1
-    # products.append(Product)
     with open('Products.pkl', 'ab') as f:
         pickle.dump(p1, f)
     print("Product added")
